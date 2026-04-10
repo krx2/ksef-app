53301cbafc8b75f93582bf43c003e87d997bdde7
Konrad Zebro <konrad.zebro@gmail.com>
2026-04-10 15:07:10 +0200

    add simple docker setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   async rewrites() {
     const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080';
     return [
